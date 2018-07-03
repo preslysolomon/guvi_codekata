4e942b22261272e2649f201600fd0bffc2fa0e2c
preslysolomon <40797784+preslysolomon@users.noreply.github.com>
2018-07-03 21:53:20 +0530

    Add files via upload

---
+++ code 2.py
@@ -0,0 +1,16 @@
+
+try:
+    a = int(input(" enter your number between 1 and 100000 " ))
+except Exception:
+    print " invalid input"
+input 
+if a%2 == 0:
+     print " even "
+
+
+
+else :
+  print "odd "
+ 
+    
+
