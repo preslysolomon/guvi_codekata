4e982ad09083fecceec324d6d71c9d7e21cc948d
preslysolomon <40797784+preslysolomon@users.noreply.github.com>
2018-07-03 20:33:20 +0530

    Add files via upload

---
+++ code 1.py
@@ -0,0 +1,17 @@
+a = int(input(" enter your number" ))
+input 
+if a < 0:
+     print " negative"
+
+elif a==0:
+    print "zero"
+
+elif a > 100000:
+    print " enter a number between 1 and 100000 "
+    
+
+else :
+  print "positive"
+ 
+    
+
