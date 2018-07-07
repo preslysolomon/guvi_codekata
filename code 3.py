de2ca57625221d530a4a56062c6d7ae11e42ef36
preslysolomon <40797784+preslysolomon@users.noreply.github.com>
2018-07-07 21:58:43 +0530

    Add files via upload

---
+++ code 3.py
@@ -0,0 +1,16 @@
+
+a =  (raw_input(" enter a letter " ))
+
+if a in ['a','e','i','o','u']:
+    
+     print " vowel"
+
+elif a in ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']:
+     print " consonent "
+
+else:
+      print"invalid input"
+
+
+    
+
