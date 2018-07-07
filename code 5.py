8da723489ad412dab10864b492ba69a238ff72b1
preslysolomon <40797784+preslysolomon@users.noreply.github.com>
2018-07-07 22:55:22 +0530

    Add files via upload

---
+++ code 5.py
@@ -0,0 +1,19 @@
+
+a =(input(" enter first number "))
+    
+b =(input(" enter second number "))
+
+c =(input(" enter third number " ))
+     
+if(a>b and a>c): 
+    
+     print " the largest number is " ,a
+
+elif(c>b and c>a):
+     
+      print c, " is the largest number " 
+
+else :
+     print b, " is the largest number  " 
+     
+
